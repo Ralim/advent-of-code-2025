b4d4923a8bc60ae466149e9ae2dd42c3416f39e3
Ben V. Brown <Ralim@Ralimtek.com>
2025-12-06 13:14:08 +1100

    Template remaining days

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,3 +1,40 @@
+use shared::{ChallengeDay, Question, get_question_data_lines};
 fn main() {
-    println!("Hello, world!");
+    let t_a = std::thread::spawn(|| {
+        let ans = part_a(Question::Question);
+        println!("Part A:{ans}");
+    });
+    let t_b = std::thread::spawn(|| {
+        let ans = part_b(Question::Question);
+        println!("Part B:{ans}");
+    });
+    t_a.join().unwrap();
+    t_b.join().unwrap();
+}
+
+fn part_a(question: Question) -> u32 {
+    println!("Starting Part A");
+    let _input_file = get_question_data_lines(ChallengeDay::Day7, question);
+    0
+}
+
+fn part_b(question: Question) -> usize {
+    println!("Starting Part B");
+    let _input_file = get_question_data_lines(ChallengeDay::Day7, question);
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_a_question1_sample() {
+        assert_eq!(part_a(Question::Sample), 0);
+    }
+
+    #[test]
+    fn test_part_b_question2_sample() {
+        assert_eq!(part_b(Question::Sample), 0);
+    }
 }
